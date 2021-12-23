25350918ea60e903caac1f592502308150104fb8
Hadiwasito1905 <noverryambo@gmail.com>
2021-12-23 09:06:41 +0700

    Cek Upload

---
+++ src/main/java/HRISproject/HRISproject/HrisProjectApplication.java
@@ -0,0 +1,13 @@
+package HRISproject.HRISproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrisProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrisProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/HRISproject/HRISproject/HrisProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package HRISproject.HRISproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrisProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
